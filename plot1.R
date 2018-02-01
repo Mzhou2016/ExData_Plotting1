d9e70673e7d6490f045da37ef46022111fc62351
Miranda Zhou <Mzhou2016@users.noreply.github.com>
2018-02-01 16:39:39 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+> setwd("C:/Users/mzou/datasciencecoursera/L4 course project1")
+> getwd()
+[1] "C:/Users/mzou/datasciencecoursera/L4 course project1"
+> t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+> t$Date <- as.Date(t$Date, "%d/%m/%Y")
+> t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+> dateTime <- paste(t$Date, t$Time)
+> hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
